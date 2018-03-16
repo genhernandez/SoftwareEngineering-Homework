e5ef9b64f97b97f16666013fb690fa3635c40fb3
Genesis Hernandez <ghernandez@mills.edu>
2018-03-15 18:03:06 -0700

    add google authentication

--- rotten_potatoes/config/routes.rb
+++ rotten_potatoes/config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+
+
+  get 'sessions/create'
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :movies
-    root :to => redirect('/movies')
+  resources :movies
+  get 'auth/:provider/callback' => 'sessions#create'
+  get 'auth/failure' => redirect('/')
+  get 'signout' => 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+  root :to => redirect('/movies')
 end
